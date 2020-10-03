bc640cf85c3d6dd392491645d83140f2c21861d0
Adam Lehechka <ajlehechka@gmail.com>
2020-10-03 13:01:31 -0500

    Initial Commit

---
+++ server/src/index.ts
@@ -0,0 +1,54 @@
+import { ApolloServer } from 'apollo-server-express';
+import connectSqlite3 from 'connect-sqlite3';
+import express from 'express';
+import session from 'express-session';
+import 'reflect-metadata';
+import { buildSchema } from 'type-graphql';
+import { createConnection, getConnectionOptions } from 'typeorm';
+import { AuthResolver } from './resolvers/AuthResolver';
+import { BookResolver } from './resolvers/BookResolver';
+
+// I like to use redis for this: https://github.com/tj/connect-redis
+const SQLiteStore = connectSqlite3(session);
+
+const server = async () => {
+	const app = express();
+
+	app.use(
+		session({
+			store: new SQLiteStore({
+				db: 'database.sqlite',
+				concurrentDB: true,
+			}),
+			name: 'qid',
+			secret: process.env.SESSION_SECRET || 'aslkdfjoiq12312',
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				httpOnly: true,
+				secure: process.env.NODE_ENV === 'production',
+				maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+			},
+		})
+	);
+
+	// get options from ormconfig.js
+	const dbOptions = await getConnectionOptions(process.env.NODE_ENV || 'development');
+	await createConnection({ ...dbOptions, name: 'default' });
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [AuthResolver, BookResolver],
+			validate: false,
+		}),
+		context: ({ req, res }) => ({ req, res }),
+	});
+
+	apolloServer.applyMiddleware({ app, cors: false });
+	const port = process.env.PORT || 4000;
+	app.listen(port, () => {
+		console.log(`server started at http://localhost:${port}/graphql`);
+	});
+};
+
+void server();
